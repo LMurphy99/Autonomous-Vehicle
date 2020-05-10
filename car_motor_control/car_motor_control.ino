9e0e7122f139f4f007ce5de7c1e0eb95129f3509
rossm31 <milligan.ross@hotmail.co.uk>
2020-05-10 17:02:08 +0100

    Motor Controller Code

---
+++ car_motor_control/car_motor_control.ino
@@ -0,0 +1,92 @@
+//  DC Motor Control with PWM for H-Bridge board L298N 
+//  By Ross Milligan 
+//  21/02/2020
+
+//connections for left wheels motor
+#define IN1 PA12    //2 //(D2)
+#define IN2 PB7     //4 //(D4)
+#define ENA PB0     //3 //(D3)
+
+//connections for right wheels motor
+#define IN3 PC14    //7 //(D7)
+#define IN4 PB1     //6 //(D6)
+#define ENB PB6     //5 //(D5)
+
+//could be defined as global - currently passed to functions
+//int PWM_Value = 0;  //a value between 0 and 255 for PWM control
+
+//function prototypes
+void stopCar();
+void forward(int);
+void backward(int);
+void left(int);
+void right(int);
+
+
+void setup() {
+  // set L298N connections as outputs
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(ENA, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+  pinMode(ENB, OUTPUT);
+}
+
+void loop() {
+//forward(255);
+//delay(1000);
+
+//locomotion(270,500);
+
+right(125);
+delay(500.4);
+forward(255);
+delay(250);
+
+//
+//forward(125);
+//delay(1000);
+//
+//right(125);
+//delay(500);
+//
+//forward(125);
+//delay(1000);
+//
+//right(125);
+//delay(500);
+//
+//forward(125);
+//delay(1000);
+//
+//right(125);
+//delay(500);
+//
+//forward(125);
+//delay(1000);
+//
+//right(125);
+//delay(500);
+//
+//forward(125);
+//delay(1000);
+//
+//left(125);
+//delay(500);
+//
+//forward(125);
+//delay(1000);
+//
+//left(125);
+//delay(500);
+//
+//forward(125);
+//delay(1000);
+//
+//left(125);
+//delay(500);
+
+stopCar();
+delay(10000);
+}
---
+++ car_motor_control/locomotion.ino
@@ -0,0 +1,38 @@
+//move car
+
+void locomtotion(float angle, float distance){
+  
+  //move by incoming angle degrees 
+  if (angle <= 180){                      //angle 0-180 deg
+    left(125);
+    delay(5.56*angle);
+  }
+  else {                                  //angle above 180 deg
+    right(125);
+    delay(5.56*angle);
+  }
+
+  //move by incoming distance mm
+  if (distance <= 160){
+    if (distance < 80){
+      forward(70);                          //distance below 80mm
+      delay(distance*12.5);
+      stopCar();
+      return;
+    }
+    forward(((-80+distance)*0.6875)+70);    //distance between 80 & 160mm
+    delay(1000);
+    return;
+  }
+  if (distance > 2000){
+    forward(255);                           //distance above 2000mm
+    delay(distance*0.5);
+    stopCar();
+    return;
+  }
+  forward(((-160+distance)*0.071)+125);     //distance above 160mm & below 2000mm
+  delay(1000);
+  stopCar();
+  return;
+}
+
